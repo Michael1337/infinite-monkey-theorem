8ce9aefac6b1e876a1870c60722dbf566a11c1d2
Michael Weber <michael.weber1337@gmail.com>
2021-11-11 23:55:27 +0100

    Add game feature and increase code quality

--- src/components/game.vue
+++ src/components/game.vue
@@ -2,8 +2,11 @@
   <div>
     <h1>{{ msg }}</h1>
     <button v-on:click="click">click</button>
+    <br>
+    <button v-on:click="upgrade">upgrade auto clicker (costs {{ auto_clicker_update_cost }} points)</button>
+    <br>
     Points: {{ points }}<br>
-    Clicked {{ clicks }} times!<br>
+    Clicked {{ total_clicks }} times!<br>
 
     Latest letter: {{ latest_letter }} <br>
     Current stack: {{ current_word }} <br>
@@ -12,66 +15,116 @@
       {{ word[0] + ":" + word[1] }}
     </div>
     <br>
-    <div v-for="letter in alphabet_map" :key="'letter_'+letter[0]">
+    <div v-for="letter in letters_found" :key="'letter_'+letter[0]">
       {{ letter[0] + ":" + letter[1] }}
     </div>
   </div>
 </template>
 
 <script>
-import dict from '../assets/words.txt';
+import dict from '@/assets/words.txt';
+
+const CURRENT_STACK_MAX_LENGTH = 20;
+const MINIMUM_WORD_LENGTH = 3;
+const MAXIMUM_WORD_LENGTH = 7;
 
 export default {
-  name: 'HelloWorld',
+  name: 'Game',
   data: function () {
     return {
-      clicks: 0,
-      current_stack: [],
-      latest_letter: '',
-      words_found: new Map(),
+      total_clicks: 0,
       points: 0,
-      alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ü', 'ö', 'ä', 'ß', ' '],
-      alphabet_map: new Map(),
-      dictionary: []
+      auto_click_level: 0,
+      alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ü', 'ö', 'ä', 'ß'],
+      dictionary: [],
+      dictionary_regexp: '',
+      latest_letter: '',
+      current_stack: [],
+      letters_found: new Map(),
+      words_found: new Map(),
+      auto_clicker: ''
     }
   },
   props: {
     msg: String
   },
   mounted() {
-    this.initAlphabetMap();
-    this.dictionary = dict.split("\r\n").filter(word => word.length <= 7 && word.length > 2);
-    console.log(this.dictionary);
-    setInterval(this.click, 1);
+    this.initLettersFound();
+    this.initDictionaryMap();
   },
   computed: {
     current_word: function () {
       return this.current_stack.join("");
+    },
+    auto_clicker_update_cost: function () {
+      return Math.floor(10 * Math.pow(1.2, this.auto_click_level));
     }
   },
   methods: {
-    initAlphabetMap: function () {
-      this.alphabet.forEach(letter => this.alphabet_map.set(letter, 0));
+    initLettersFound: function () {
+      this.alphabet.forEach(letter => this.letters_found.set(letter, 0));
     },
-    click: function () {
-      ++this.clicks;
-      this.latest_letter = this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
-      this.current_stack.push(this.latest_letter);
-      if (this.current_stack.length > 20) {
+    initDictionaryMap: function () {
+      this.dictionary = dict.split("\r\n").filter(word => word.length <= MAXIMUM_WORD_LENGTH && word.length >= MINIMUM_WORD_LENGTH);
+      this.dictionary_regexp = "(" + this.dictionary.join("|").toLowerCase() + ")$";
+    },
+    updateAutoClicker: function () {
+      clearInterval(this.auto_clicker);
+      if (this.auto_click_level > 0) {
+        this.auto_clicker = setInterval(() => {
+          this.click();
+        }, 1000 / this.auto_click_level);
+      }
+    },
+    getRandomLetterFromAlphabet: function () {
+      return this.alphabet[Math.floor(Math.random() * this.alphabet.length)];
+    },
+    updateCurrentStack: function (new_letter) {
+      this.current_stack.push(new_letter);
+
+      if (this.current_stack.length > CURRENT_STACK_MAX_LENGTH) {
         this.current_stack.shift();
       }
-      this.alphabet_map.set(this.latest_letter, this.alphabet_map.get(this.latest_letter) + 1);
+    },
+    incrementLetterFound: function (latest_letter) {
+      this.letters_found.set(latest_letter, this.letters_found.get(latest_letter) + 1);
+    },
+    currentStackIncludesAnyWord: function () {
+      return new RegExp(this.dictionary_regexp).test(this.current_word);
+    },
+    awardPointsForLetter: function () {
+      this.points++;
+    },
+    awardPointsForWord: function (previous_count, word) {
+      // on first find, award 10 times as many points
+      this.points += previous_count === 0 ? 10 * Math.pow(10, word.length - 2) : 10 * Math.pow(10, word.length - 3);
+    },
+    incrementWordFound: function (word, previous_count) {
+      this.words_found.set(word, (previous_count + 1));
+    },
+    click: function () {
+      this.total_clicks++;
+      this.latest_letter = this.getRandomLetterFromAlphabet();
+      this.awardPointsForLetter();
 
-      const reg = "(" + this.dictionary.join("|").toLowerCase() + ")$";
-      if (new RegExp(reg).test(this.current_word)) {
-        //found a word
+      this.updateCurrentStack(this.latest_letter);
+      this.incrementLetterFound(this.latest_letter);
+
+      if (this.currentStackIncludesAnyWord()) {
         const word = this.dictionary.filter(word => this.current_word.includes(word)).pop();
 
         if (typeof word !== "undefined") {
           const previous_count = this.words_found.get(word) ?? 0;
-          this.words_found.set(word, (previous_count + 1));
-          this.points += previous_count === 0 ? Math.pow(10, word.length - 1) : Math.pow(10, word.length - 2);
+          this.incrementWordFound(word, previous_count);
+          this.awardPointsForWord(previous_count, word);
         }
+      }
+    },
+    upgrade: function () {
+      if (this.points >= this.auto_clicker_update_cost) {
+        this.points -= this.auto_clicker_update_cost;
+        this.auto_click_level++;
+        this.updateAutoClicker();
       }
     }
   }
